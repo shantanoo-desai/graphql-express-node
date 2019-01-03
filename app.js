1d865518dd6779290206272b62ec468c776d434d
Shantanoo Desai <shantanoo.desai@gmail.com>
2019-01-03 11:44:12 +0100

    Add GraphQL Schema Skeleton
    - `buildSchema` uses string to define schema for GraphQL
    - default schema acc. to Specification is `schema { query: RootQuery  mutation: RootMutation}`
       - `query`: fetch data, `mutation`: alter data (delete, create, update)
    - add `type` to schema to tell what actually will be returned
    
    Signed-off-by: Shantanoo Desai <shantanoo.desai@gmail.com>

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 const express = require('express'); // Add Express Module
 const bodyParser = require('body-parser'); // Add Body-Parser Middleware for JSON handling in Requests
 const graphqlHttp = require('express-graphql'); // Add Middleware for GraphQL Resolvers over Express HTTP
+const { buildSchema } = require('graphql'); // Javascript Object-Destructuring (pull objects from packages)
 
 const app = express();
 
 app.use(bodyParser.json()); // JSON parsing Middleware added
 
 app.use('/graphql', graphqlHttp({
-    schema: null,
+    schema: buildSchema(`
+        type RootQuery {
+        }
+        
+        type RootMutation {
+        }
+        
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
     rootValue: {}
 }));
 
