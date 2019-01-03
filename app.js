b69fa2008b491d7f7c0b2a66718f668d9e4010cc
Shantanoo Desai <shantanoo.desai@gmail.com>
2019-01-03 11:22:09 +0100

    Add Basic Express Skeleton App
    - Simple `Hello World` check on Port 3000 for App
    - Bump version in `package.json`
    - Add `package-lock.json`
    
    Signed-off-by: Shantanoo Desai <shantanoo.desai@gmail.com>

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
+const express = require('express'); // Add Express Module
+const bodyParser = require('body-parser'); // Add Body-Parser Middleware for JSON handling in Requests
 
+const app = express();
+
+app.use(bodyParser.json()); // JSON parsing Middleware added
+
+app.use('/', (req, res, next) => {
+   res.send('Hello World!');
+});
+
+app.listen(3000);
