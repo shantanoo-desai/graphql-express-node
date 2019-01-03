529bd3447aaee40490f4ebfe2ae900a163e5daad
Shantanoo Desai <shantanoo.desai@gmail.com>
2019-01-03 11:35:04 +0100

    Add Initial GraphQL Middleware with Express
    - GraphQL: Single Endpoint `/graphql` where POST will be sent
    - within Middleware `schema` is where the GraphQL schema will be
    - within Middleware `rootValue` is where our GraphQL resolver will be
    
    Signed-off-by: Shantanoo Desai <shantanoo.desai@gmail.com>

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express'); // Add Express Module
 const bodyParser = require('body-parser'); // Add Body-Parser Middleware for JSON handling in Requests
+const graphqlHttp = require('express-graphql'); // Add Middleware for GraphQL Resolvers over Express HTTP
 
 const app = express();
 
 app.use(bodyParser.json()); // JSON parsing Middleware added
 
-app.get('/', (req, res, next) => {
-   res.send('Hello World!');
-});
+app.use('/graphql', graphqlHttp({
+    schema: null,
+    rootValue: {}
+}));
 
 app.listen(3000);
