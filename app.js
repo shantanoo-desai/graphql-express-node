0915b0edf05a74dcf3ca6d0a4eb21ab657a455c5
Shantanoo Desai <shantanoo.desai@gmail.com>
2019-01-08 18:51:41 +0100

    Add `User` mutation for GraphQL Schema
    - create `User` type, correspoding Input type and
      mutation `createUser`
    - Never return the password from the DB
    - Avoid duplicating the user entry into the DB using `findOne`
    
    Signed-off-by: Shantanoo Desai <shantanoo.desai@gmail.com>

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const bodyParser = require('body-parser'); // Add Body-Parser Middleware for JSO
 const graphqlHttp = require('express-graphql'); // Add Middleware for GraphQL Resolvers over Express HTTP
 const { buildSchema } = require('graphql'); // Javascript Object-Destructuring (pull objects from packages)
 const mongoose = require('mongoose'); // MongoDB Third-Party package
-
+const bcrypt = require('bcryptjs'); // password hashing
 
 const Event = require('./models/event'); // MongoDB Event Model
-
+const User = require('./models/user'); // MongoDB User Model
 const app = express();
 
 app.use(bodyParser.json()); // JSON parsing Middleware added
@@ -19,6 +19,17 @@ app.use('/graphql', graphqlHttp({
             description: String!
             price: Float!
             date: String!
+        }
+        
+        type User {
+            _id: ID!
+            email: String!,
+            password: String
+        }
+        
+        input UserInput {
+            email: String!
+            password: String!
         }
         
         input EventInput {
@@ -34,6 +45,7 @@ app.use('/graphql', graphqlHttp({
         
         type RootMutation {
             createEvent(eventInput: EventInput): Event
+            createUser(userInput: UserInput): User
         }
         
         schema {
@@ -67,7 +79,7 @@ app.use('/graphql', graphqlHttp({
                 .save()
                 .then(result => {
                     console.log(result);
-                    return { ...result._doc };
+                    return { ...result._doc, _id: result._doc._id.toString() };
                     // return result._doc
                 })
                 .catch(err => {
@@ -75,6 +87,34 @@ app.use('/graphql', graphqlHttp({
                     throw err;
                 });
 
+        },
+        createUser: (args) => {
+            return User.findOne({ email: args.userInput.email})
+                .then(user => {
+                    if(user) {
+                        throw new Error('User Exists Already');
+                    }
+                    return bcrypt.hash(args.userInput.password, 12)
+                        .then(hashedPassword => {
+                            const user = new User({
+                                email: args.userInput.email,
+                                password: hashedPassword
+                            });
+                            return user.save()
+                                .then(result => {
+                                    return {...result._doc, password: null, _id: result.id};
+                                })
+                                .catch(err => {
+                                    throw err;
+                                })
+                        })
+                        .catch(err => {
+                            throw err;
+                        });
+                })
+                .catch(err => {
+                    throw err;
+                });
         }
     },
     graphiql: true
