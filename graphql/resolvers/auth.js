3da8cbd50a95121c30009c4d4b9722b9d5a29e26
Shantanoo Desai <shantanoo.desai@gmail.com>
2019-05-16 21:49:47 +0200

    Add Authentication Logic with JWT
    Signed-off-by: Shantanoo Desai <shantanoo.desai@gmail.com>

--- graphql/resolvers/auth.js
+++ graphql/resolvers/auth.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcryptjs'); // password hashing
 const User = require('../../models/user'); // MongoDB User Model
+const jwt = require('jsonwebtoken');
 
 module.exports = {
     createUser: async (args) => {
@@ -19,5 +20,21 @@ module.exports = {
         catch (e) {
             throw e;
         }
+    },
+    login: async ({email, password}) => {
+        const user = await User.findOne({email: email});
+        if(!user) {
+            throw new  Error('User does not Exist');
+        }
+        const isEqual = await bcrypt.compare(password, user.password);
+
+        if (!isEqual) {
+            throw new Error('Invalid Credentials');
+        }
+
+        const token = jwt.sign({userId: user.id, email: user.email}, 'somesupersecretkey',
+            {expiresIn: '1h'});
+
+        return {userId: user.id, token: token, tokenExpiration: 1};
     }
 };
