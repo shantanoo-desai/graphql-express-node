c14ce38ae16c44c66bf64fa058b4b169397eaaed
Shantanoo Desai <shantanoo.desai@gmail.com>
2019-08-12 21:36:38 +0200

    Initial GraphQL Mutation try
    - Without CORS adaptation in Backend
    
    Signed-off-by: Shantanoo Desai <shantanoo.desai@gmail.com>

--- frontend/src/app/auth/auth.component.ts
+++ frontend/src/app/auth/auth.component.ts
@@ -12,13 +12,27 @@ export class AuthComponent implements OnInit {
 
   model = new User('', '');
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
   }
 
   onSubmit() {
-    console.log(this.model);
+    // console.log(this.model);
+    const requestBody = {
+      query: `
+        mutation {
+          createUser(userInput: {email: "${this.model.email}", password: "${this.model.password}"}) {
+            _id
+            email
+          }
+        }
+      `
+    };
+    this.authService.addUser(requestBody)
+      .subscribe(result => {
+        console.log(result);
+      });
   }
 
 }
