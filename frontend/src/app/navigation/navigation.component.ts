65b7421a6d239729be66baf0f7608dedda5c7e0d
Shantanoo Desai <shantanoo.desai@gmail.com>
2020-01-29 22:52:40 +0100

    Add Navigation UI logic and Log Out

--- frontend/src/app/navigation/navigation.component.ts
+++ frontend/src/app/navigation/navigation.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-navigation',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavigationComponent implements OnInit {
 
-  constructor() { }
+  constructor(private auth: AuthService, private router: Router) { }
 
   ngOnInit() {
   }
 
+  get isLoggedIn(): boolean {
+    return (this.auth.currentUser && this.auth.getToken()) ? true : false;
+  }
+
+  logOut() {
+    this.auth.logOut();
+  }
 }
